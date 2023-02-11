701edc43e68d894aab63f8c2699b220bf3002314
André Carlos <95436218+iwaandore@users.noreply.github.com>
2023-02-11 00:49:20 -0300

    Delete main.js

--- s2/main.js
+++
@@ -1,7 +0,0 @@
-const frases = document.querySelectorAll("#frases li");
-const botao = document.querySelector("#gerarFrase");
-
-botao.addEventListener("click", function () {
-    const numeroAleatorio = Math.floor(Math.random() * frases.length);
-    document.getElementById("fraseEscolhida").innerHTML = frases[numeroAleatorio].innerHTML;
-});
